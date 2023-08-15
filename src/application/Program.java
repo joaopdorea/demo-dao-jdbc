58714a901c711df54b107de0300d351fe1654464
Joao Pedro <joaodorea300@gmail.com>
2023-08-15 14:41:05 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.Department;
+import model.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller s1 = new Seller(21, "Joao", "joaodorea@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(s1);
 
 	}
 
