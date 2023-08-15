1ad2e28506f123fdcd8f9f1263b37c1b257a4dde
Joao Pedro <joaodorea300@gmail.com>
2023-08-15 14:35:03 -0300

    Department entity class

---
+++ src/model/Seller.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.sql.Date;
+
+public class Seller {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private double BaseSalary;
+	
+	
+}
